5be08787a2623b144eeda844ddaaa892b40dc6ca
dcep93 <dcep93@gmail.com>
2024-11-06 17:29:21 -0500

    x

--- app/react420/src/react420/pages/deployLocks/index.tsx
+++ app/react420/src/react420/pages/deployLocks/index.tsx
@@ -7,10 +7,10 @@ const data = data_raw as unknown as {
   slack: SlackMessage[];
 };
 
-// git log --since=@$(date -v-180d +%s 2>/dev/null || date -d "180 days ago" +%s) --pretty=format:'{"hash": "%H", "timestamp": %ct}' --reverse | jq -c -s .
-
 export default function DeployLocks() {
-  const maxAge = 180;
+  const start = 1714968000;
+  const windowSizeSeconds = 60 * 60 * 24;
+
   const mappedSlack = data.slack.map((d) => ({
     ...d,
     timestamp: parseFloat(d.ts),
@@ -75,13 +75,10 @@ export default function DeployLocks() {
       }
     ).locks;
   const today = new Date(new Date().toLocaleDateString());
-  const days = Array.from(new Array(maxAge))
-    .map((_, i) => maxAge - i)
-    .map((age) => {
-      const d = new Date(today);
-      d.setUTCDate(today.getUTCDate() - age);
-      return d.getTime() / 1000;
-    })
+  const windows = Array.from(
+    new Array(Math.ceil((today.getTime() / 1000 - start) / windowSizeSeconds))
+  )
+    .map((_, i) => start + windowSizeSeconds * i)
     .map((timestamp) => ({
       timestamp,
       lockHours: parseFloat(
@@ -89,7 +86,7 @@ export default function DeployLocks() {
           .filter(
             (l) =>
               l.timestamp >= timestamp &&
-              l.timestamp <= timestamp + 60 * 60 * 24
+              l.timestamp <= timestamp + windowSizeSeconds
           )
           .map((l) => l.duration / (60 * 60))
           .reduce((a, b) => a + b, 0)
@@ -98,7 +95,8 @@ export default function DeployLocks() {
       commits: gitData
         .filter(
           (l) =>
-            l.timestamp >= timestamp && l.timestamp <= timestamp + 60 * 60 * 24
+            l.timestamp >= timestamp &&
+            l.timestamp <= timestamp + windowSizeSeconds
         )
         .map((l) => l.duration / (60 * 60))
         .filter((d) => d < 24)
@@ -106,44 +104,42 @@ export default function DeployLocks() {
     }))
     .map((d) => ({
       ...d,
-      lockHours:
-        d.lockHours === 0 ? undefined : d.lockHours > 12 ? -1 : d.lockHours,
+      lockHours: d.lockHours === 0 ? undefined : Math.min(12, d.lockHours),
       numCommits: d.commits.length,
       p50: d.commits[Math.floor(d.commits.length * 0.5)],
       p90: d.commits[Math.floor(d.commits.length * 0.9)],
       max: d.commits[d.commits.length - 1],
     }));
-  console.log(days);
+  console.log(windows);
   return (
     <div>
       <div>
         <pre style={{ whiteSpace: "pre-wrap" }}>
           {SlackFetcher(
             (s) => s.user === "U01ABDUQKTN",
-            (s) => s.ts <= "1699294562"
+            (s) => s.ts <= start.toString()
           )}
         </pre>
+        <pre>{`git log --since=@${start} --pretty=format:'{"hash": "%H", "timestamp": %ct}' --reverse | jq -c -s .`}</pre>
       </div>
       <div>
         {["lockHours", "numCommits", "p50", "p90", "max"].map((dataKey) => (
           <div key={dataKey}>
             <h1>{dataKey}</h1>
             <div>
-              <LineChart data={days} width={1200} height={300}>
+              <LineChart data={windows} width={1200} height={500}>
                 <XAxis
                   dataKey={"timestamp"}
                   type={"number"}
                   scale={"time"}
-                  domain={[days[0]?.timestamp]}
+                  domain={[windows[0]?.timestamp]}
                   tickFormatter={(tick) =>
                     new Date(tick * 1000).toLocaleDateString()
                   }
                 />
                 <YAxis />
                 <Tooltip
-                  labelFormatter={(tick) =>
-                    new Date(tick * 1000).toDateString()
-                  }
+                  labelFormatter={(tick) => new Date(tick * 1000).toString()}
                 />
                 <Line type="linear" dataKey={dataKey} />
               </LineChart>
